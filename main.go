19f9c3233c9c7a6191020acc447492b936315c37
Tran Hau <ngtranhau@gmail.com>
2020-04-02 02:36:59 +0700

    vendor

--- main.go
+++ main.go
@@ -22,6 +22,12 @@ func main() {
 	r.GET("/ping", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "ping")
 	})
+	r.POST("/collect", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "collect")
+	})
+	r.POST("/summary", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "summary")
+	})
 
 	if err := r.Run(fmt.Sprintf(":%s", port)); err != nil {
 		log.Fatal(err)
