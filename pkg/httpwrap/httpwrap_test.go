0ab7f0feef3784b8b909e6e3ea8e95c805aec21e
Tran Hau <ngtranhau@gmail.com>
2020-05-31 04:29:00 +0700

    unittest

---
+++ pkg/httpwrap/httpwrap_test.go
@@ -0,0 +1,43 @@
+package httpwrap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddParams(t *testing.T) {
+	tests := []struct {
+		name        string
+		originalURL string
+		parans      []Param
+		wantResult  string
+		wantError   error
+	}{
+		{
+			name:        "success",
+			originalURL: "https://www.google.com/",
+			parans: []Param{
+				{
+					Name:  "a",
+					Value: "1",
+				},
+				{
+					Name:  "b",
+					Value: "2",
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			gotResult, gotError := AddParams(tc.originalURL, tc.parans...)
+			if tc.wantError != nil {
+				assert.Equal(t, tc.wantError, gotError)
+				return
+			}
+			assert.Equal(t, tc.wantResult, gotResult)
+		})
+	}
+}
